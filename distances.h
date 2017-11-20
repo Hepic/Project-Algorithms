eceffc14fc375dac3be0c03dd455846328036f9a
Hepic <hepic_@hotmail.com>
2017-11-20 09:26:47 +0200

    Implement Mean Frechet

--- distances.h
+++ distances.h
@@ -9,8 +9,11 @@
 
 using namespace std;
 
+static Curve default_curve;
+
 double euclidean_distance(const vector<double>&, const vector<double>&);
-double discrete_frechet_distance(const Curve&, const Curve&);
+vector<double> mean_point(const vector<double>&, const vector<double>&);
+double discrete_frechet_distance(const Curve&, const Curve&, Curve& = default_curve, bool = false);
 double dynamic_time_wrapping(const Curve&, const Curve&);
 double compute_distance(const Curve&, const Curve&, const char*);
 
