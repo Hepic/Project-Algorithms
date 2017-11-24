d5ce5337ca5843c7a030ffbdf7c4679718e709f7
Hepic <hepic_@hotmail.com>
2017-11-24 12:20:18 +0200

    Improve PAM

--- cluster.h
+++ cluster.h
@@ -1,14 +1,16 @@
 #ifndef CLUSTER_H
 #define CLUSTER_H
 
+#include "curve.h"
 #include <vector>
 
 using namespace std;
 
 vector<int> k_random_selection(int, int);
 vector<int> k_means_pp(int, int, const char*);
-double loyd_assignment(const vector<int>&, vector<int>&, vector<int>&);
-double PAM_update(const vector<int>&, const vector<int>&, const vector<int>&, double, int&, int&);
-void clustering(vector<int>&);
+double loyd_assignment(const vector<int>&, vector<int>&, vector<double>&, vector<double>&);
+bool PAM_update(vector<int>&, const vector<int>&, const vector<double>&, const vector<double>&, double);
+double mean_frechet(vector<Curve>&, const vector<int>&);
+void clustering();
 
 #endif
