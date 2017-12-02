925d88b9281510546578584de7dd307e2cc8c462
Hepic <hepic_@hotmail.com>
2017-12-02 19:30:42 +0200

    Improve DTW

--- distances.cpp
+++ distances.cpp
@@ -65,10 +65,9 @@ double discrete_frechet_distance(const Curve &curve_1, const Curve &curve_2, Cur
     
     for (int i = 1; i < N1; ++i) {
         for (int j = 1; j < N2; ++j) {
-            double dist = euclidean_distance_square(curve_1.get_point(i), curve_2.get_point(j));
-            double val = -1;
+            dist = euclidean_distance_square(curve_1.get_point(i), curve_2.get_point(j));
+            double val = min(min(dp_solve[i - 1][j], dp_solve[i][j - 1]), dp_solve[i - 1][j - 1]);
 
-            val = min(min(dp_solve[i - 1][j], dp_solve[i][j - 1]), dp_solve[i - 1][j - 1]);
             dp_solve[i][j] = max(val, dist);
         }
     }
@@ -117,42 +116,35 @@ double dynamic_time_wrapping(const Curve &curve_1, const Curve &curve_2) {
     int N1 = curve_1.get_length();
     int N2 = curve_2.get_length();
     double **dp_solve = new double*[N1];
-    double result;
+    double result, dist;
 
     for (int i = 0; i < N1; ++i) {
         dp_solve[i] = new double[N2];
+    }
 
-        for (int j = 0; j < N2; ++j) {
-            dp_solve[i][j] = 0;
+    dist = euclidean_distance_square(curve_1.get_point(0), curve_2.get_point(0));
+    dp_solve[0][0] = dist;
+    
+    for (int i = 1; i < N1; ++i) {
+        dist = euclidean_distance_square(curve_1.get_point(i), curve_2.get_point(0));
+        dp_solve[i][0] = dp_solve[i - 1][0] + dist;
+    }
+    
+    for (int i = 1; i < N2; ++i) {
+        dist = euclidean_distance_square(curve_1.get_point(0), curve_2.get_point(i));
+        dp_solve[0][i] = dp_solve[0][i - 1] + dist;
+    }
+    
+    for (int i = 1; i < N1; ++i) {
+        for (int j = 1; j < N2; ++j) {
+            dist = euclidean_distance_square(curve_1.get_point(i), curve_2.get_point(j));
+            double val = min(min(dp_solve[i - 1][j], dp_solve[i][j - 1]), dp_solve[i - 1][j - 1]);
+
+            dp_solve[i][j] = dist + val;
         }
     }
     
-    for (int i = 0; i < N1; ++i) {
-        for (int j = 0; j < N2; ++j) {
-            double dist = euclidean_distance_square(curve_1.get_point(i), curve_2.get_point(j));
-            double val = -1;
-
-            if (i-1 >= 0) {
-                val = dp_solve[i-1][j];
-            }
-
-            if (j-1 >= 0) {
-                val = (val == -1 ? dp_solve[i][j-1] : min(val, dp_solve[i][j-1]));
-            }
-            
-            if (i-1 >= 0 && j-1 >= 0) {
-                val = (val == -1 ? dp_solve[i-1][j-1] : min(val, dp_solve[i-1][j-1]));
-            }
-           
-            dp_solve[i][j] = dist;
-            
-            if (val != -1) {
-                dp_solve[i][j] += val;
-            }
-        }
-    }
-    
-    result = sqrt(dp_solve[N1-1][N2-1]);
+    result = sqrt(dp_solve[N1 - 1][N2 - 1]);
     
     for (int i = 0; i < N1; ++i) {
         delete[] dp_solve[i];
